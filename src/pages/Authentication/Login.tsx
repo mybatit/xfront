8bd377d1704d13c978d545d709560bf4ad5e83a9
FarhanMohammadDev <farhanmohammad0677@gmail.com>
2024-10-05 01:18:19 +0100

    ajouter Logo dans page login

--- src/pages/Authentication/Login.tsx
+++ src/pages/Authentication/Login.tsx
@@ -82,12 +82,21 @@ export default function SignUpPage() {
       <div className="flex-1 flex items-center justify-center">
         <div className="w-full max-w-md space-y-8 px-4">
           <div className="text-center">
-            <h2 className="text-3xl font-bold text-gray-900">Se connecter</h2>
-            <p className="mt-2 text-sm text-gray-600">
-              Bienvenue sur la plateforme de ( VR )
+            <p className="mb-2 text-sm text-gray-600">
+              Bienvenue sur
+              <img
+                src="/Vengo-reserve.png"
+                alt="Praxedo Logo"
+                width={200}
+                height={50}
+                className="h-10 mx-auto"
+              />
             </p>
+
           </div>
           <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <h2 className="text-3xl text-center font-bold text-gray-900">Se connecter</h2>
+
             <div className="space-y-4">
               <div>
                 <label htmlFor="email" className="sr-only">
@@ -127,7 +136,9 @@ export default function SignUpPage() {
               <Checkbox
                 id="agree-terms"
                 checked={agreedToTerms}
-                onCheckedChange={(checked) => setAgreedToTerms(checked as boolean)}
+                onCheckedChange={(checked) =>
+                  setAgreedToTerms(checked as boolean)
+                }
               />
               <label
                 htmlFor="agree-terms"
@@ -138,22 +149,22 @@ export default function SignUpPage() {
             </div>
 
             <div>
-              {loading ? 
-                <Button disabled 
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                
+              {loading ? (
+                <Button
+                  disabled
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 >
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Please wait
-              </Button> : 
-
-              <Button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-              >
-                Se connecter
-              </Button>
-            }
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </Button>
+              ) : (
+                <Button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Se connecter
+                </Button>
+              )}
             </div>
           </form>
         </div>
