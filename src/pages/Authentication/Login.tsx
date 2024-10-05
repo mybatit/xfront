493cd92b40d2689c33032885c80ba3773cbd721e
FarhanMohammadDev <farhanmohammad0677@gmail.com>
2024-10-05 15:32:39 +0100

    ajouter login page responsive ✨

--- src/pages/Authentication/Login.tsx
+++ src/pages/Authentication/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from "react"; 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -14,7 +14,6 @@ export default function SignUpPage() {
   const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
-
   const { toast } = useToast();
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -69,14 +68,12 @@ export default function SignUpPage() {
   }
 
   return (
-    <div className="flex min-h-screen bg-gray-50">
-      <div className="flex-1 flex items-center justify-center p-10 bg-white">
+    <div className="flex flex-col md:flex-row min-h-screen bg-gray-50">
+      <div className="flex-1 hidden md:flex items-center justify-center p-10 bg-white">
         <img
           src="/images/640.gif"
           alt="3D illustration of data and cloud computing elements"
-          width={600}
-          height={600}
-          className="max-w-full h-auto"
+          className="max-w-full h-auto w-[300px] md:w-[600px]"
         />
       </div>
       <div className="flex-1 flex items-center justify-center">
@@ -87,15 +84,14 @@ export default function SignUpPage() {
               <img
                 src="/Vengo-reserve.png"
                 alt="Praxedo Logo"
-                width={200}
-                height={50}
                 className="h-10 mx-auto"
               />
             </p>
-
           </div>
           <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-          <h2 className="text-3xl text-center font-bold text-gray-900">Se connecter</h2>
+            <h2 className="text-3xl text-center font-bold text-gray-900">
+              Se connecter
+            </h2>
 
             <div className="space-y-4">
               <div>
@@ -108,7 +104,7 @@ export default function SignUpPage() {
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  className="relative block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-500 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                   placeholder="E-mail"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
@@ -124,7 +120,7 @@ export default function SignUpPage() {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                  className="relative block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-500 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                   placeholder="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
@@ -152,7 +148,7 @@ export default function SignUpPage() {
               {loading ? (
                 <Button
                   disabled
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  className="w-full flex justify-center py-2 px-4 rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 >
                   <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                   Please wait
@@ -160,7 +156,7 @@ export default function SignUpPage() {
               ) : (
                 <Button
                   type="submit"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  className="w-full flex justify-center py-2 px-4 rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 >
                   Se connecter
                 </Button>
